64e820f70e89c493c6a21fc982a4fea64e0b540b
nexon5 <dm.lychahin@gmail.com>
2021-08-20 09:29:23 +0300

    update

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ let mainArea = document.querySelector("ul");
 document.querySelector("button").onclick = function () {
     let text = field.value;
     if (text !== "") {
-        /*mainArea.innerHTML += `<li><input type="checkbox"><span>${text}</span></li>`;
-        console.log(mainArea.innerHTML);*/
         let newElem = document.createElement("li");
         newElem.innerHTML = `<input type="checkbox"><span>${text}</span>`;
         mainArea.appendChild(newElem);
